4e22b303c01dfbb24f0c86a198dcbf1648926d20
Mikhail Subach <scorpibear@tut.by>
2018-09-04 18:50:48 +0300

    support of events, getFirst, load, toJSON, toList

--- queue.js
+++ queue.js
@@ -1,25 +1,59 @@
-class Queue {
+const EventEmitter = require('events').EventEmitter;
+
+const comparator = (theItem) => 
+  (item => 
+    theItem.fen == item.fen && 
+    ( theItem.depth ? (theItem.depth <= item.depth) : true)
+  );
+
+class Queue extends EventEmitter {
   constructor() {
+    super();
     this.priorities = 4;
     this.values = [[],[],[],[]];
+    this.Console = console;
+  }
+  get events() {
+    return {empty: 'empty', change: 'change'};
+  }
+  get size() {
+    return this.values.reduce((accumulator, shelf) => accumulator + shelf.length);
   }
   add(item, priority = 0) {
-    if(!this.getItem(item.fen)) {
+    if(item && item.fen && item.depth && !this.get(item)) {
+      // need to delete even if this.get didn't returns anything,
+      // because we can have an item with lower depth but the same fen
+      this.delete(item.fen);
+      this.emitChangeEvent();
       this.values[priority].push(item);
+      return this.getPlace(item);
+    } else {
+      this.Console.error(`Could not add '${item}' to queue`);
+      return -1;
     }
   }
-  deleteItem(fen) {
+  delete(fen) {
     for(let p = 0; p < this.priorities; p++) {
       let i = this.values[p].findIndex(item => item.fen == fen);
       if(i >= 0){
         this.values[p].splice(i, 1);
+        this.emitChangeEvent();
+        if(this.size == 0) {
+          this.emitEmptyEvent();
+        }
         return;
       }
     }
   }
-  getItem(fen) {
+  emitChangeEvent() {
+    this.emit(this.events.change, this.toJSON());
+  }
+  emitEmptyEvent() {
+    this.emit(this.events.empty);
+  }
+  get(theItem) {
     for(let p = 0; p < this.priorities; p++) {
-      let item = this.values[p].find(item => item.fen == fen);
+      let item = this.values[p].find(comparator(theItem));
       if(item) {
         return item;
       }
@@ -28,6 +62,36 @@ class Queue {
   }
   getAllItems() {
     return [].concat(...this.values);
+  }
+  getFirst() {
+    for(let p = 0; p < this.priorities; p++) {
+      if(this.values[p].length) {
+        return this.values[p][0];
+      }
+    }
+    return null;
+  }
+  getPlace(theItem) {
+    let order = 0;
+    for(let p = 0; p < this.priorities; p++) {
+      let i = this.values[p].findIndex(comparator(theItem));
+      if(i != -1) {
+        order += i;
+        return order;
+      }
+      order = this.values[p].length;
+    }
+    return -1;
+  }
+  load(values) {
+    this.values = values;
+    this.emitChangeEvent();
+  }
+  toJSON() {
+    return JSON.parse(JSON.stringify(this.values));
+  }
+  toList() {
+    return this.getAllItems();
   }
 }
 
--- spec/queue.spec.js
+++ spec/queue.spec.js
@@ -1,25 +1,173 @@
 describe('queue', () => {
   const Queue = require('../queue');
-  const item = {fen: '8/PKRpppk1/8/8/8/8/8/8', moves: ['d4'], depth: 100};
+  const depth = 100;
+  const fen = '8/PKRpppk1/8/8/8/8/8/8';
+  const item = {fen, moves: ['d4'], depth};
+  const handler = {onEmpty: () => {}, onChange: () => {}};
+  const consoleStub = {log:()=>{}, error:()=>{}};
+  let queue;
+  beforeEach(() => {
+    queue = new Queue();
+  });
   it('what is added could be get', () => {
-    const queue = new Queue();
     queue.add(item);
-    expect(queue.getItem(item.fen)).toBe(item);
+    expect(queue.get(item)).toBe(item);
+  });
+  it('toJSON output could be loaded', () => {
+    queue.add(item);
+    queue.add({fen: 'p2', depth}, 2);
+    queue.add({fen: 'p1', depth}, 1);
+    const json = queue.toJSON();
+    const newQueue = new Queue();
+    newQueue.load(json);
+    expect(queue.getAllItems()).toEqual(newQueue.getAllItems());
   });
   describe('add', () => {
     it('does not allow to add the item with the same fen twice', () => {
-      const queue = new Queue();
       queue.add(item);
       queue.add(item);
       expect(queue.getAllItems()).toEqual([item]);
     });
-  });
-  describe('deleteItem', () => {
-    it('deletes added item', () => {
-      const queue = new Queue();
+    it('emits change event', () => {
+      spyOn(handler, 'onChange');
+      queue.on('change', handler.onChange);
       queue.add(item);
-      queue.deleteItem(item.fen);
-      expect(queue.getItem(item.fen)).toBeNull();
+      expect(handler.onChange).toHaveBeenCalled();
+    });
+    it('does not emits change event when adding the same fen', () => {
+      spyOn(handler, 'onChange');
+      queue.on('change', handler.onChange);
+      queue.add(item);
+      queue.add(item);
+      expect(handler.onChange).toHaveBeenCalledTimes(1);
+    });
+    it('place item with P1 earlier than P2', () => {
+      queue.add({fen: 'p2', depth}, 2);
+      queue.add({fen: 'p1', depth}, 1);
+      expect(queue.getAllItems()).toEqual([{fen: 'p1', depth}, {fen: 'p2', depth}]);
+    });
+    it('place items with the same priority at the end of queue', () => {
+      queue.add({fen: 'first', depth});
+      queue.add({fen: 'second', depth});
+      expect(queue.getAllItems()).toEqual([{fen: 'first', depth}, {fen: 'second', depth}]);
+    });
+    it('empty fens are not added', () => {
+      queue.add({fen: '', depth});
+      expect(queue.getAllItems()).toEqual([]);
+    });
+    it('does not allow to add items with empty depth', () => {
+      spyOn(consoleStub, 'error');
+      queue.Console = consoleStub;
+      queue.add({fen});
+      expect(queue.getAllItems()).toEqual([]);
+      expect(consoleStub.error).toHaveBeenCalled();
+    });
+    it('replaces item if the same fen is added with greater depth', () => {
+      queue.add({fen, depth: 90});
+      queue.add({fen, depth: 100});
+      expect(queue.getAllItems()).toEqual([{fen, depth: 100}]);
+    });
+    it('returns place in queue', () => {
+      spyOn(queue, 'getPlace').and.returnValue(42);
+      expect(queue.add(item)).toBe(42);
+    });
+    it('emits change event with json value as param', () => {
+      spyOn(queue, 'toJSON').and.returnValue([{a: 1, b: 2}]);
+      spyOn(handler, 'onChange');
+      queue.on('change', handler.onChange);
+      queue.add(item);
+      expect(handler.onChange).toHaveBeenCalledWith([{a: 1, b: 2}]);
+    });
+  });
+  describe('delete', () => {
+    it('deletes added item', () => {
+      queue.add(item);
+      queue.delete(item.fen);
+      expect(queue.get(item)).toBeNull();
+    });
+    it('emits empty event if the queue is empty', () => {
+      queue.add(item);
+      spyOn(handler, 'onEmpty');
+      queue.on('empty', handler.onEmpty);
+      queue.delete(item.fen);
+      expect(handler.onEmpty).toHaveBeenCalled();
+    });
+    it('emits change event in case of delete occured', () => {
+      queue.add(item);
+      queue.add({fen: item.fen + 'r', depth});
+      spyOn(handler, 'onChange');
+      queue.on('change', handler.onChange);
+      queue.delete(item.fen);
+      expect(handler.onChange).toHaveBeenCalled();
+    });
+    it('does not emit empty event if there are more left', () => {
+      queue.add(item);
+      queue.add({fen: item.fen + 'p', moves: ['a4', 'a5'], depth});
+      spyOn(handler, 'onEmpty');
+      queue.on('empty', handler.onEmpty);
+      queue.delete(item.fen);
+      expect(handler.onEmpty).not.toHaveBeenCalled();
+    });
+  });
+  describe('get', () => {
+    it('returns null for not-existent fen', () => {
+      expect(queue.get({fen: 'something'})).toBeNull();
+    });
+    it('returns null if fen are matched but requested depth is higher', () => {
+      queue.add({fen, depth: 40});
+      expect(queue.get({fen, depth: 100})).toBeNull();
+    });
+    it('returns item in queue if fen match and depth requested is lower than in queue', () => {
+      queue.add(item);
+      expect(queue.get({fen, depth: 90})).toEqual(item);
+    });
+    it('returns item by fen only in case depth is not specified in request', () => {
+      queue.add(item);
+      expect(queue.get({fen})).toEqual(item);
+    });
+  });
+  describe('getFirst', () => {
+    it('returns the first element from the queue', () => {
+      queue.add(item);
+      expect(queue.getFirst()).toBe(item);
+    });
+    it('returns the first element from p3, if p0, p1, p2 are empty', () => {
+      queue.add(item, 3);
+      expect(queue.getFirst()).toBe(item);
+    });
+    it('does not change queue', () => {
+      queue.add(item);
+      queue.getFirst();
+      expect(queue.getAllItems()).toEqual([item]);
+    });
+  });
+  describe('getPlace', () => {
+    it('returns -1 if not in queue', () => {
+      expect(queue.getPlace(item)).toBe(-1);
+    });
+    it('returns 0 for the first place', () => {
+      queue.add(item);
+      expect(queue.getPlace(item)).toBe(0);
+    });
+    it('returns -1 if depth in queue is lower', () => {
+      queue.add({fen, depth: 40});
+      expect(queue.getPlace({fen, depth: 100})).toBe(-1);
+    });
+    it('works if depth in queue is higher', () => {
+      queue.add({fen, depth: 100});
+      expect(queue.getPlace({fen, depth: 99})).toBe(0);
+    });
+  });
+  describe('load', () => {
+    it('loads new queue', () => {
+      queue.load([[{ fen: 'new', depth: 50 }],[],[],[]]);
+      expect(queue.getAllItems()).toEqual([{ fen: 'new', depth: 50 }]);
+    });
+    it('emits change event', () => {
+      spyOn(handler, 'onChange');
+      queue.on('change', handler.onChange);
+      queue.load([[item],[],[],[]]);
+      expect(handler.onChange).toHaveBeenCalled();
     });
   });
 });
